8411e1efad3f44869fc5b5e57bd37bc69f150003
fekoneko <55813967+fekoneko@users.noreply.github.com>
2024-11-24 22:30:31 +0300

    port previous code to the project

--- src/main/java/ru/obninsk/iate/easycipher/Main.java
+++ src/main/java/ru/obninsk/iate/easycipher/Main.java
@@ -1,7 +1,17 @@
 package ru.obninsk.iate.easycipher;
 
+import javax.swing.*;
+
 public class Main {
+    private final static int INITIAL_WIDTH = 1000;
+    private final static int INITIAL_HEIGHT = 800;
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SwingUtilities.invokeLater(() -> {
+            var mainWindow = new MainWindow();
+            mainWindow.setBounds(0, 0, INITIAL_WIDTH, INITIAL_HEIGHT);
+            mainWindow.setLocationRelativeTo(null);
+            mainWindow.setVisible(true);
+        });
     }
 }
---
+++ src/main/java/ru/obninsk/iate/easycipher/MainWindow.java
@@ -0,0 +1,12 @@
+package ru.obninsk.iate.easycipher;
+
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+    private JPanel contentPane;
+
+    public MainWindow() {
+        setTitle("EasyCipher");
+        setContentPane(contentPane);
+    }
+}
