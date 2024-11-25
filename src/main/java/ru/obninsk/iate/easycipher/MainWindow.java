1851607598f418353e7245cdcadda1698bc857ee
fekoneko <55813967+fekoneko@users.noreply.github.com>
2024-11-26 01:38:07 +0300

    add dummy choose button handler

--- src/main/java/ru/obninsk/iate/easycipher/MainWindow.java
+++ src/main/java/ru/obninsk/iate/easycipher/MainWindow.java
@@ -1,16 +1,24 @@
 package ru.obninsk.iate.easycipher;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+// TODO: Localize / move out all the hardcoded strings in form
 
 public class MainWindow extends JFrame {
     private JPanel contentPane;
     private JTextPane titleTextPane;
-    private JButton chooserPanelButton;
     private JPanel chooserPanel;
     private JLabel chooserPanelLabel;
+    private JButton chooserPanelButton;
 
     public MainWindow() {
         setTitle("EasyCipher");
         setContentPane(contentPane);
+        chooserPanelButton.addActionListener(this::handleChooserPanelButtonClick);
+    }
+
+    private void handleChooserPanelButtonClick(ActionEvent event) {
+        System.out.println("handleChooserPanelButtonClick");
     }
 }
