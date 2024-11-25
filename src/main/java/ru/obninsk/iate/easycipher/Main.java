9bc4116ee865e4be3bc0e36037db93ba84dc55ef
fekoneko <55813967+fekoneko@users.noreply.github.com>
2024-11-26 00:57:48 +0300

    initial screen design

--- src/main/java/ru/obninsk/iate/easycipher/Main.java
+++ src/main/java/ru/obninsk/iate/easycipher/Main.java
@@ -1,17 +1,35 @@
 package ru.obninsk.iate.easycipher;
 
 import javax.swing.*;
+import java.awt.*;
+import java.io.InputStream;
 
 public class Main {
-    private final static int INITIAL_WIDTH = 1000;
-    private final static int INITIAL_HEIGHT = 800;
+    private final static int INITIAL_WIDTH = 600;
+    private final static int INITIAL_HEIGHT = 600;
 
     public static void main(String[] args) {
+        maybeLoadFonts();
         SwingUtilities.invokeLater(() -> {
             var mainWindow = new MainWindow();
             mainWindow.setBounds(0, 0, INITIAL_WIDTH, INITIAL_HEIGHT);
             mainWindow.setLocationRelativeTo(null);
             mainWindow.setVisible(true);
         });
+    }
+
+    private static void maybeLoadFonts() {
+        InputStream monospaceFontStream =
+                Main.class.getResourceAsStream("/fonts/jetbrains-mono-font.ttf");
+
+        try {
+            if (monospaceFontStream == null) throw new Exception();
+            var monospaceFont = Font.createFont(Font.TRUETYPE_FONT, monospaceFontStream);
+            UIManager.put("Label.font", monospaceFont.deriveFont(Font.PLAIN, 14));
+            UIManager.put("TextPane.font", monospaceFont.deriveFont(Font.PLAIN, 14));
+            UIManager.put("Button.font", monospaceFont.deriveFont(Font.PLAIN, 14));
+        } catch (Exception e) {
+            System.out.println("Error occurred while loading fonts");
+        }
     }
 }
--- src/main/java/ru/obninsk/iate/easycipher/MainWindow.java
+++ src/main/java/ru/obninsk/iate/easycipher/MainWindow.java
@@ -4,6 +4,10 @@ import javax.swing.*;
 
 public class MainWindow extends JFrame {
     private JPanel contentPane;
+    private JTextPane titleTextPane;
+    private JButton chooserPanelButton;
+    private JPanel chooserPanel;
+    private JLabel chooserPanelLabel;
 
     public MainWindow() {
         setTitle("EasyCipher");
