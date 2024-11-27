f60e0919593b50cefd7850cacde34ddb1586ecd1
fekoneko <55813967+fekoneko@users.noreply.github.com>
2024-11-27 22:33:00 +0300

    feat: make algorithm select and description working

--- src/main/java/ru/obninsk/iate/easycipher/routes/EncryptRoute.java
+++ src/main/java/ru/obninsk/iate/easycipher/routes/EncryptRoute.java
@@ -1,6 +1,8 @@
 package ru.obninsk.iate.easycipher.routes;
 
+import ru.obninsk.iate.easycipher.lib.Algorithm;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 import java.io.File;
 import java.net.URL;
 
@@ -9,14 +11,24 @@ public class EncryptRoute extends Route {
     private JPanel openedItemPanel;
     private JLabel openedItemPanelLabel;
     private JPanel algorithmPanel;
-    private JComboBox algorithmPanelComboBox;
     private JLabel algorithmPanelLabel;
+    private JComboBox<String> algorithmPanelComboBox;
+    private JTextPane algorithmPanelDescription;
+
     private final File targetItem;
+    private Algorithm selectedAlgorithm = Algorithm.AES;
+    private static final String[] ALGORITHM_OPTIONS = { "AES", "Blowfish", "Twofish" };
+    private static final String[] ALGORITHM_DESCRIPTIONS = {
+            "AES Description, AES Description, AES Description, AES Description, AES Description.",
+            "Blowfish Description, Blowfish Description, Blowfish Description, Blowfish Description.",
+            "Twofish Description, Twofish Description, Twofish Description, Twofish Description.",
+    };
 
     public EncryptRoute(File targetItem) {
         super("EasyCipher - " + targetItem.getName());
         this.targetItem = targetItem;
         renderOpenedItemLabelPanel();
+        algorithmPanelComboBox.addActionListener(this::handleAlgorithmPanelComboBoxAction);
     }
 
     @Override
@@ -38,5 +50,20 @@ public class EncryptRoute extends Route {
         } catch (Exception e) {
             System.err.println("Error occurred while loading icon");
         }
+    }
+
+    private void createUIComponents() {
+        algorithmPanelComboBox = new JComboBox<>(ALGORITHM_OPTIONS);
+    }
+
+    private void handleAlgorithmPanelComboBoxAction(ActionEvent event) {
+        int selectedIndex = algorithmPanelComboBox.getSelectedIndex();
+
+        if (selectedIndex == 0) selectedAlgorithm = Algorithm.AES;
+        else if (selectedIndex == 1) selectedAlgorithm = Algorithm.BLOWFISH;
+        else selectedAlgorithm = Algorithm.TWOFISH;
+
+        algorithmPanelDescription.setText(ALGORITHM_DESCRIPTIONS[selectedIndex]);
+        SwingUtilities.invokeLater(() -> algorithmPanelDescription.revalidate());
     }
 }
