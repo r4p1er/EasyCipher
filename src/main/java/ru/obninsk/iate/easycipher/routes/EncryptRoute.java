613a885b7f26e8aa326bf763a0b8347c4d4d446f
fekoneko <55813967+fekoneko@users.noreply.github.com>
2024-12-08 21:28:53 +0300

    fix: always save the encrypted file to the recent

--- src/main/java/ru/obninsk/iate/easycipher/routes/EncryptRoute.java
+++ src/main/java/ru/obninsk/iate/easycipher/routes/EncryptRoute.java
@@ -115,7 +115,7 @@ public class EncryptRoute extends Route {
     private void handleEncryptButtonAction(ActionEvent event) {
         var mainFrame = MainFrame.getInstance();
         mainFrame.showNotification("Item encrypted successfully");
-        mainFrame.addToRecentItems(targetItem);
+        mainFrame.addToRecentItems(new File(destinationPath));
         mainFrame.navigate(new StartRoute());
     }
 
